f9a3e22813bb6adcf4b30023d94c220293fba415
lujov <69487916+lujov@users.noreply.github.com>
2020-09-18 11:58:23 -0300

    Trabajos arq

---
+++ Programacion Web/Clase 1/js/script.js
@@ -0,0 +1,31 @@
+
+// EJERCICIO 2
+// declaracion de variables de usuario
+// let nombre = prompt ("Ingresa tu nombre");
+// let apellido = prompt ("Ingresa tu apellido");
+// alert ("Bienvenido a nuestra página " +nombre+" "+apellido);
+
+// EJERCICIO 3
+
+// creación de variables
+// let numero1;
+// let numero2;
+// let resultado;
+
+// // ingreso de datos
+// numero1 = parseInt(prompt("Ingresa el primer numero"));
+// numero2 = parseInt(prompt("Ingresa el segundo numero"));
+// resultado = numero1+numero2;
+// alert("El resultado es "+resultado);               
+
+// EJERCICIO 4
+
+// declaración de variables
+let numero;
+let numerotriple;
+
+// Ingreso de valores
+numero = parseInt(prompt("Ingresa el numero a multiplicar"));
+numerotriple = numero*3;
+alert("El resultado es "+numerotriple);
+
